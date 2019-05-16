1a75e273330790464c6727cfdf8eb9c86a9a83d4
wanwan1997 <996607679@qq.com>
2019-05-16 16:42:59 +0800

    初始化

---
+++ student_demo/src/com/nnxy/moder/Model.java
@@ -0,0 +1,33 @@
+package com.nnxy.moder;
+
+public class Model {
+ private int id;
+ private String name;
+ private int sex;
+ 
+public Model(int id, String name, int sex) {
+	super();
+	this.id = id;
+	this.name = name;
+	this.sex = sex;
+}
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public int getSex() {
+	return sex;
+}
+public void setSex(int sex) {
+	this.sex = sex;
+}
+ 
+}
